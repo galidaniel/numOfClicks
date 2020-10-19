50598220e136a6c0c1522b8230d0664a38a9ed70
Gal Daniel <galidanielwork@gmail.com>
2020-10-19 13:13:09 +0300

    added connection text view

--- app/src/main/java/com/example/numofclicks/MainActivity.java
+++ app/src/main/java/com/example/numofclicks/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=(TextView) findViewById(R.id.txtResult);
     }
 
     public void methodToDo(View view) {
